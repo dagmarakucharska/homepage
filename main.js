85c8865fb4f09fef89f6962cca0c92b572374533
dagmarakucharska <carribean09@gmail.com>
2020-01-19 13:46:49 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+alert('Witaj na stronie!');
+
+console.log('witojcie!');
+
+const name ='Daga';
+const age = 28;
+
+console.log(name);
+console.log(age);
+
+console.log(`Nazywam się ${name} i mam ${age} lat`);
